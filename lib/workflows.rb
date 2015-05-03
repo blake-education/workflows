14816cb917360dfa993b2e06439409f464b64378
Lachie Cox <lachiec@gmail.com>
2015-05-03 11:50:06 +1000

    include the right files

--- lib/workflows.rb
+++ lib/workflows.rb
@@ -1,7 +1,10 @@
 require "workflows/version"
-require "workflows/workflow"
-require "workflows/service"
+
 require "workflows/error"
+require "workflows/service"
+
+require "workflows/workflow"
+require "workflows/generic_workflow"
 
 module Workflows
 end
