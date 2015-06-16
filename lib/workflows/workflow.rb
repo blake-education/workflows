acda5527bbcb91d0a8913124341cc162842dd2a7
Brenton Annan <brentonannan@brentonannan.com>
2015-06-16 12:33:18 +1000

    Revert "Allow services that call a number of services"

--- lib/workflows/workflow.rb
+++ lib/workflows/workflow.rb
@@ -47,7 +47,33 @@ module Workflows
     # ```
     #
     def call_each(*fns, failure:, success:)
-      call ::Workflows::Service.compose_with_error_handling(*fns), failure: failure, success: success
+      call compose_with_error_handling(*fns), failure: failure, success: success
+    end
+
+    # Composes a list of functions into a single function.
+    #
+    # *Note* that this isn't a general purpose composition.
+    # *Note* a function here is anything that responds to `call` i.e. lambda or a singleton module.
+    #
+    # The composed function and each constituent function have the same signature, roughly:
+    # ```
+    # fn :: void -> ReturnValue
+    # ```
+    #
+    # *Note* that `void` is used here to mean "takes no arguments".
+    # This is a dead giveaway that functions of this type signature will have side effects.
+    #
+    def compose_with_error_handling(*fns)
+      fns.flatten.compact.inject do |composed, fn|
+        -> {
+          last_return = composed.call
+          if Workflows::Error.error?(last_return)
+            last_return
+          else
+            fn.arity > 0 ? fn.curry[last_return] : fn.call
+          end
+        }
+      end
     end
   end
 end
--- spec/workflows/workflow_spec.rb
+++ spec/workflows/workflow_spec.rb
@@ -73,4 +73,53 @@ describe Workflows::Workflow do
       expect(messages).to eq([:failed])
     end
   end
+
+  context '.compose_with_error_handling' do
+    it 'composes lambdas' do
+      handled = []
+
+      r = subject.compose_with_error_handling(
+        -> { handled << :a; "first" },
+        [ 
+          -> { handled << :b; "array[0]" },
+          nil,
+          -> { handled << :c; "array[1]" },
+        ],
+        -> { handled << :d; "last" }
+      ).call
+
+      expect(handled).to eq([:a, :b, :c, :d])
+      expect(r).to eq("last")
+    end
+
+    it 'composes lambdas with arity > 0 ala function composition' do
+      handled = []
+
+      r = subject.compose_with_error_handling(
+        -> { handled << :a; "first" },
+        [
+          ->(input) { handled << :b; "array[0] #{input}" },
+          nil,
+          ->(input) { handled << :c; "array[1] #{input}" },
+        ],
+        ->(input) { handled << :d; "last #{input}" }
+      ).call
+
+      expect(handled).to eq([:a, :b, :c, :d])
+      expect(r).to eq("last array[1] array[0] first")
+    end
+
+    it 'composes error handling' do
+      handled = []
+      r = subject.compose_with_error_handling(
+        -> { handled << :a; "first" },
+        -> { handled << :b; Workflows::ErrorValue.new("error") },
+        -> { handled << :c; "last" }
+      ).call
+
+      expect(handled).to eq([:a, :b])
+      expect(r).to be_instance_of(Workflows::ErrorValue)
+      expect(r.value).to eq("error")
+    end
+  end
 end
